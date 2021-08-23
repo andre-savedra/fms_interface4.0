23838587c3e7f2b5c95c5f4beeb5d9f6f2581ed2
andre-savedra <andresavedracruz@gmail.com>
2021-08-23 13:07:13 -0300

    corrigido java path

--- java/com/spring/fms/config/InterfaceConfig.java
+++ java/com/spring/fms/config/InterfaceConfig.java
@@ -5,5 +5,5 @@ public class InterfaceConfig {
 	public static int CLOUD_TYPE = 0;
 	public static int LOCAL_TYPE = 1;
 	
-	public static int interfaceType = LOCAL_TYPE;
+	public static int interfaceType = CLOUD_TYPE;
 }
--- src/main/resources/static/js/config.js
+++ src/main/resources/static/js/config.js
@@ -1,7 +1,11 @@
 const CLOUD_TYPE = 0;
 const LOCAL_TYPE = 1;
 
-const interfaceType = LOCAL_TYPE;
+const interfaceType = CLOUD_TYPE;
+const defaultUrl = "";
 
-const defaultUrl = "http://localhost:8080/"; //use this just for server mode
-//const defaultUrl  = "http://fms-interface.us-east-1.elasticbeanstalk.com/";
+if (interfaceType == CLOUD_TYPE) {
+    defaultUrl = "http://fms-interface.us-east-1.elasticbeanstalk.com/";
+} else {
+    defaultUrl = "http://localhost:8080/"; //use this just for server mode
+}
